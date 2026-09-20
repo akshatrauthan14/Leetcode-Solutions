1ccbe124fcbeafeb535de633fd9c9edbfea841ad
Akshat Rauthan <akshatrauthan14@gmail.com>
2026-09-20 21:17:06 +0530

    Create 3498. Reverse Degree of a String.java

---
+++ strings/3498. Reverse Degree of a String.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int sum = 0;
+        for(int i = 0; i<s.length(); i++){
+            int val = s.charAt(i)-'a'+1;
+            int rev = 27-val;
+            sum += rev*(i+1);
+        }
+        return sum;
+    }
+}
